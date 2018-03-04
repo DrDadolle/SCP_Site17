b14852a12c74a2918a7521f3387a8f3bc285efaa
Dr Dadolle <alexandre.dadolle@gmail.com>
2018-03-04 18:18:40 +0100

    BBM : finished wall. still 2 bugs left

--- Assets/Scripts/CreateWall.cs
+++ Assets/Scripts/CreateWall.cs
@@ -8,19 +8,29 @@ public class CreateWall : MonoBehaviour {
 	public static CreateWall Instance;
 
 	//Indicate if currently build a wall of not
-	bool creatingWall;
+	bool dragAndDropping;
 
-	Vector3 startPosition;
-
+	//Store the starting position of the drag and drop
+	private Vector3 startPosition;
 
 	private Vector3 it;
 	private Vector3 lastIteration;
 
-	//Show the starting point of the building
+	/*keep in memory which axis we were building on;
+	 * 0 = none
+	 * 1 = X
+	 * 2 = Z
+	 */
+	private int buildingAxis = 0;
+
+	//Show the starting point of the wall
 	private GameObject startPole;
 
-	// Array of currenly created gameObject
-	private GameObject[] standbyGO;
+	//Show the ending point of the wall
+	private GameObject endPole;
+
+	// Array of provisiorary wall's GO
+	private List<GameObject> standbyGO;
 
 	//Public game objects models
 	public GameObject polePrefab;
@@ -28,10 +38,10 @@ public class CreateWall : MonoBehaviour {
 
 
 
-
 	//Awake
 	void Awake(){
 		Instance = this;
+		standbyGO = new List<GameObject> ();
 	}
 
 
@@ -44,9 +54,9 @@ public class CreateWall : MonoBehaviour {
 			Instance.setWall();
 		}
 
-		if(Instance.creatingWall){
+		if(Instance.dragAndDropping){
 			Instance.updateWall(pointer);
-			
+
 		}
 	}
 
@@ -58,7 +68,8 @@ public class CreateWall : MonoBehaviour {
 		// Set the starting Position
 		Vector3 startPos = pointer.getWorldPoint();
 		startPosition = pointer.snapPosition (startPos);
-		creatingWall = true;
+		dragAndDropping = true;
+		lastIteration = startPosition;
 
 		// Show the starting position with a gameObject
 		startPole = Instantiate (polePrefab, startPosition, Quaternion.identity);
@@ -67,9 +78,19 @@ public class CreateWall : MonoBehaviour {
 
 	//Effectively create the wall
 	void setWall(){
-		Destroy (startPole);
-		creatingWall = false;
+
+		if (lastIteration.Equals (startPosition)) {
+			Destroy (Instance.startPole);
+			Destroy (Instance.endPole);
+		}
+
+		//Clear the previous arraylist
+		standbyGO.Clear();
+
+		//Finish Drag And drop
+		dragAndDropping = false;
 	}
+
 
 	//Show the future position of the wall
 	void updateWall(ShowMousePosition pointer){
@@ -79,78 +100,165 @@ public class CreateWall : MonoBehaviour {
 		currentPosition = pointer.snapPosition (currentPosition);
 		currentPosition = new Vector3 (currentPosition.x, currentPosition.y, currentPosition.z);
 
-
-		if (!currentPosition.Equals (startPosition)) {
-			recursiveWallCreation(startPosition, currentPosition);
+		if (!currentPosition.Equals(lastIteration)) {
+			recursiveWallBuilder(startPosition, currentPosition);
+			Debug.Log ("current = " + currentPosition.x);
+			Debug.Log ("last = " + lastIteration.x);
 		}
 
 	}
 
-	//Create effectively a Segment of the Wall
-	void createWallSegment(int iteration, bool createOnX, bool isPositive){
 
-		if (!isPositive)
-			iteration = -iteration;
-
-		if (createOnX) {
-			it = new Vector3 (startPosition.x + iteration, startPosition.y, startPosition.z);
-		} else {
-			it = new Vector3 (startPosition.x, startPosition.y, startPosition.z + iteration);
-		}
-
-
-		//Get the middle between the 2 last points
-		Vector3 middle = Vector3.Lerp (lastIteration, it, 0.5f);
-		GameObject newWall = Instantiate (wallPrefab, middle, Quaternion.identity);
-
-		// Rotate the model so the forward axis points in the right direction
-		Vector3 YRotation = new Vector3(0, 90, 0);
-
-		newWall.transform.LookAt (startPole.transform);
-		newWall.transform.Rotate (YRotation);
-
-		//Update the last iteration
-		lastIteration = it;
-
-	}
-
-	/*
+	/**
+	 * Create recursively walls between a start position and an end position
 	 * StartPos and finishPos are both snapped already
 	 * 
 	 */
-	void recursiveWallCreation(Vector3 startPos, Vector3 finishPos)
+	void recursiveWallBuilder(Vector3 startPos, Vector3 finishPos)
 	{
-		Debug.Log ("AAAAAA");
 		int _max_x = Mathf.RoundToInt (finishPos.x - startPos.x);
 		int max_x = Mathf.Abs (_max_x); 
 		int _max_z = Mathf.RoundToInt (finishPos.z - startPos.z);
 		int max_z = Mathf.Abs (_max_z); 
 
-		Debug.Log ("BBBBB");
-
-		//if it is an x-building
-		//Default case
-		if (max_x != 0 && max_x >= max_z) {
-			lastIteration = startPos;
-			Debug.Log ("Entering the loop");
-			for (int i = 1; i <= max_x; i++) {
-				Debug.Log ("Inside the loop : " + i);
-				Instance.createWallSegment (i, true, _max_x > 0);
+			//if it is an x-axis building
+			//Default case
+		if (max_x >= max_z) {
+			if (max_x > standbyGO.Count) {
+				lastIteration = startPos;
+				for (int i = 0; i < max_x; i++) {
+					buildingAxis = 1;
+					Instance.createWallSegment (i + 1, _max_x > 0);
+				}
+			} else {
+				destroyObsoleteGO (_max_x);
+		
 			}
 		}
-		//else if its an y-building
-		else if(max_z != 0) {
-			lastIteration = startPos;
-			for (int i = 1; i <= max_z; i++) {
-				Instance.createWallSegment (i, false, _max_z > 0);
+		//else if its an z-axis building
+		else {
+			if (max_z > standbyGO.Count) {
+				lastIteration = startPos;
+				for (int i = 0; i < max_z; i++) {
+					buildingAxis = 2;
+					Instance.createWallSegment (i + 1, _max_z > 0);
+				}
+			} else {
+				destroyObsoleteGO (_max_z);
+			}
+		}
+	}
+
+	//Create effectively a Segment of the Wall
+	void createWallSegment(int iteration, bool isPositive){
+
+		if (!isPositive)
+			iteration = -iteration;
+
+		if (buildingAxis == 1) {
+			it = new Vector3 (startPosition.x + iteration, startPosition.y, startPosition.z);
+			onAxisChangeDestroy (1);
+		} else {
+			it = new Vector3 (startPosition.x, startPosition.y, startPosition.z + iteration);
+			onAxisChangeDestroy (2);
+		}
+
+		//Check that all the object are build on the same side (negative or positive one)
+		if (standbyGO.Count > 0) {
+			if (buildingAxis == 1) {
+				if (it.x > startPosition.x && standbyGO [0].transform.position.x < startPosition.x)
+					clearAndDestroyAllGO ();
+				else if (it.x < startPosition.x && standbyGO [0].transform.position.x > startPosition.x)
+					clearAndDestroyAllGO ();
+			} else {
+				if (it.z > startPosition.z && standbyGO [0].transform.position.z < startPosition.z)
+					clearAndDestroyAllGO ();
+				else if (it.z < startPosition.z && standbyGO [0].transform.position.z > startPosition.z)
+					clearAndDestroyAllGO ();
 			}
 		}
 
+		// checks if it is a new item
+		if (Mathf.Abs(iteration) > standbyGO.Count) {
+
+			//Get the middle between the 2 last points
+			Vector3 middle = Vector3.Lerp (lastIteration, it, 0.5f);
+
+			//Create the wall
+			GameObject newWall = Instantiate (wallPrefab, middle, Quaternion.identity);
+
+			//Add it to the arraylist
+			standbyGO.Add (newWall);
+
+			// Rotate the model so the forward axis points in the right direction
+			Vector3 YRotation = new Vector3 (0, 90, 0);
+			newWall.transform.LookAt (startPole.transform);
+			newWall.transform.Rotate (YRotation);
+
+			//Destroy previous endPole
+			if (endPole == null) {
+				endPole = Instantiate (polePrefab, it, Quaternion.identity);
+			} else if (!endPole.transform.position.Equals (it)){
+				endPole.transform.position = new Vector3 (it.x, it.y, it.z);
+			}
+
+		}
+
+		//Update the last iteration
+		lastIteration = it;
+	}
+
+	//Destroy every gameobject stored on change of axis
+	void onAxisChangeDestroy(int axis_int)
+	{
+		if (buildingAxis != axis_int) {
+			buildingAxis = axis_int;
+
+			clearAndDestroyAllGO();
+		}
+	}
+
+	//destroy
+	void destroyObsoleteGO(int pseudoMax){
+		int pseudoMaxAbs = Mathf.Abs (pseudoMax);
+
+		for (int i = pseudoMaxAbs  ; i < standbyGO.Count; i++) {
+			if (pseudoMax > 0) {
+				if (buildingAxis == 1) {
+					lastIteration.x -= 1;
+				} else {
+					lastIteration.z -= 1;
+				}
+			} else if (pseudoMax < 0) {
+				if (buildingAxis == 1) {
+					lastIteration.x += 1;
+				} else {
+					lastIteration.z += 1;
+				}
+			} else {
+				if (buildingAxis == 1) {
+					lastIteration.x = startPosition.x;
+				} else {
+					lastIteration.z = startPosition.z;
+				}
+			}
+
+			if (endPole != null) 
+				endPole.transform.position = lastIteration;
+
+
+			Destroy (standbyGO [i]);
+			standbyGO.RemoveAt (i);
+		}
 	}
 
 
-
-
-
-
+	//Utility Method
+	void clearAndDestroyAllGO()
+	{
+		standbyGO.ForEach(delegate(GameObject obj) {
+			Destroy(obj);
+		});
+		standbyGO.Clear ();
+	}
 }
