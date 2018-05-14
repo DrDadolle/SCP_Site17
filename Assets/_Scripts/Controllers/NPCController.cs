ab38716bfd5c329bc260f0259d72f85c17c5ce63
DrDadolle <alexandre.dadolle@gmail.com>
2018-05-14 17:53:48 +0200

    Adding 'Add npc' button and adding them to the manager

--- Assets/_Scripts/Controllers/NPCController.cs
+++ Assets/_Scripts/Controllers/NPCController.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+/**
+ *  Control NPCs
+ */
 public class NPCController : MonoBehaviour {
 
     public GameObject npcPrefab;
 
-    // TODO : Add button to add npc
-    
+    // Populate a NPC (for test purpose for now)
+    public void PopulateNPC()
+    {
+        //Quaternion to be changed
+        GameObject go = GameObject.Instantiate(npcPrefab, Vector3.zero, Quaternion.identity);
+        NPCManager.Instance.listOfNPCS.Add(go);
+    }
+
     //TODO : create Seriazebla NPCModel
 }
